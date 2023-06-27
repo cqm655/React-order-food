533fa666bb056abe833ac67eec0aacb814be6d9e
iurie.balan <iurie.balan@112.md>
2023-06-27 10:23:35 +0300

    reviewed useContext implementation, used useReducer and useRef

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,14 +1,14 @@
+import React from 'react';
 import classes from './Input.module.css';
 
-const Input = props => {
+const Input = React.forwardRef((props, ref) => {
     return (
         <div className={classes.input}>
             <label htmlFor={props.input.id}>{props.label}</label>
-             <input {...props.input} />  {/* /* ensure that we receive all data to input element from input.js element*/ }
-            
+             <input ref={ref} {...props.input} />  {/* /* ensure that we receive all data to input element from input.js element*/ }
         </div>
     );
-
-}
+    
+});
 
 export default Input;
