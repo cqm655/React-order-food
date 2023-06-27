5de0b223645fbb8b4d9a1053b1a8f807554a7d37
iurie.balan <iurie.balan@112.md>
2023-06-27 11:41:56 +0300

    added button effects using useEffect and useState

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,39 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import cartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(cartContext);
+  const [btnIsHighLighted, setBtnIsHighLighted] = useState(false);
+
+  const { items } = cartCtx; // use destructuring, to make bump effect only when we have some items change, and not all elements from context
 
   const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
-  }, 0) // 0 is initial value, necessarly
+  }, 0); // 0 is initial value, necessarly
+
+  const btnClasses = `${classes.button} ${
+    btnIsHighLighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnIsHighLighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighLighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer); //we clear timer after useEffect worked
+    }
+  }, [items]);
   return (
     <>
-
-      <button className={classes.button} onClick={props.onClick}>
+      <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
           <CartIcon />
         </span>
--- src/components/Meals/MeallItem/MealItemForm.js
+++ src/components/Meals/MeallItem/MealItemForm.js
@@ -1,18 +1,20 @@
-import { useRef, useState } from "react";
-import Input from "../../UI/Input";
-import classes from "./MealItemForm.module.css";
+import { useRef, useState } from 'react';
+
+import Input from '../../UI/Input';
+import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
   const amountInputRef = useRef();
 
-  const [amountIsValid, setAmountIsValid] = useState(false);
   const submitHandler = (event) => {
     event.preventDefault();
 
     const enteredAmount = amountInputRef.current.value;
     const enteredAmountNumber = +enteredAmount;
+
     if (
-      enteredAmount.trim().lenght === 0 ||
+      enteredAmount.trim().length === 0 ||
       enteredAmountNumber < 1 ||
       enteredAmountNumber > 5
     ) {
@@ -22,22 +24,23 @@ const MealItemForm = (props) => {
 
     props.onAddToCart(enteredAmountNumber);
   };
+
   return (
-    <form action="" className={classes.form} onSubmit={submitHandler}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
         ref={amountInputRef}
-        label="Amount"
+        label='Amount'
         input={{
-          id: "amount_" + props.id,
-          type: "number",
-          min: "1",
-          max: "5",
-          step: "1",
-          defaultValue: "1",
+          id: 'amount_' + props.id,
+          type: 'number',
+          min: '1',
+          max: '5',
+          step: '1',
+          defaultValue: '1',
         }}
       />
       <button>+ Add</button>
-      {!amountIsValid && <p>Please anter a valid amount</p>}
+      {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
     </form>
   );
 };
